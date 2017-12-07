01dc954ff5e0dba71bde8d437090c929b734ab11
una <una@ubivelox.com>
2017-12-07 16:53:41 +0900

    add all

---
+++ Bitcoin/Model/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Bitcoin
+//
+//  Created by una on 2017. 12. 7..
+//  Copyright © 2017년 una. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: Any {
+
+}
---
+++ Bitcoin/ViewModel/BitCoinChartViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BitCoinChartViewModel.swift
+//  Bitcoin
+//
+//  Created by una on 2017. 12. 7..
+//  Copyright © 2017년 una. All rights reserved.
+//
+
+import UIKit
+
+class BitCoinChartViewModel: Any {
+
+}
